8ec70be6cd3b08c69887e6923785d44fc312d79d
aleskxyz <39186039+aleskxyz@users.noreply.github.com>
2024-03-29 19:43:44 +0100

    Initial release

---
+++ setup_runner.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+packages=("buildah" "uidmap" "libcap2" "libcap2-bin" "podman" "qemu" "binfmt-support" "qemu-user-static")
+missing_packages=false
+
+echo "::group::Checking Package Installation"
+for pkg in "${packages[@]}"; do
+    if ! dpkg -s "$pkg" &> /dev/null; then
+        echo "::warning::Package $pkg is not installed."
+        missing_packages=true
+    else
+        echo "Package $pkg is already installed."
+    fi
+done
+echo "::endgroup::"
+
+if [ "$missing_packages" = true ]; then
+    echo "::group::Installing Missing Packages"
+    sudo apt-get update
+    sudo apt-get -y install "${packages[@]}"
+    echo "::endgroup::"
+else
+    echo "::notice::All packages are already installed."
+fi
+
+echo "::group::Applying Additional Configurations"
+sudo setcap cap_setuid+eip /usr/bin/newuidmap
+sudo setcap cap_setgid+eip /usr/bin/newgidmap
+sudo chmod u-s /usr/bin/newuidmap
+sudo chmod u-s /usr/bin/newgidmap
+echo -e "[storage]\ndriver = \"vfs\"" | sudo tee /etc/containers/storage.conf > /dev/null
+mkdir -p "$HOME/.docker"
+echo "::endgroup::"
+echo "::notice::Configuration completed successfully."
